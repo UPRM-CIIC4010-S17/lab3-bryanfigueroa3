aac049ca400e54bf1c8a1ea0cc788cbf85e88652
4015d8 <4015d8@mango.ece.uprm.edu>
2017-02-08 14:38:35 -0400

    Changes made were the following:
    Uncommented line 8 so that eclipse terminates the application
    completely.
    Changed the frame's title, size and location.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -1,11 +1,13 @@
 import javax.swing.JFrame;
 
-public class Lab3Exercises {
-	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+public class Lab3Exercises
+{
+	public static void main(String[] args)
+	{
+		JFrame myFrame = new JFrame("Bryan Figueroa");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setLocation(900, 400);
+		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 	}
 }
